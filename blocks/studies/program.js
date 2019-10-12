e1a5b8441af30183e2752ebb5299f2b7a58def08
noamoss <noam@thepitz.io>
2019-10-13 01:01:19 +0300

    improve program block

--- blocks/studies/program.js
+++ blocks/studies/program.js
@@ -1,6 +1,6 @@
 var programJson = {
   "type": "program",
-  "message0": "שם התכנית %1 %2 %3 %4 גורמים מנהלים %5 %6 גורם נותן שירות לסטודנט/ית",
+  "message0": "שם התכנית %1 %2 %3 סוג תעודה / יעד %4 %5 גורמים מנהלים %6 %7 גורם נותן שירות לסטודנט/ית %8 %9 אפיקי לימודים %10 %11 תנאי סיום %12 %13",
   "args0": [
     {
       "type": "input_dummy"
@@ -16,6 +16,17 @@ var programJson = {
       "check": "String"
     },
     {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "goal",
+      "check": "certificate"
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
       "type": "input_statement",
       "name": "admins"
     },
@@ -25,9 +36,24 @@ var programJson = {
     {
       "type": "input_statement",
       "name": "srvice"
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "afiks",
+      "check": "afik"
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "graduation_checks",
+      "check": "graduation_conditions"
     }
   ],
-  "inputsInline": true,
   "nextStatement": "afik",
   "colour": 160,
   "tooltip": "תכנית תוגדר על פי סוג היעד או התעודה המוענקים בסופה. במסגרת כל תכנית יוגדר אפיק, או אפיקים שונים שיאפשרו לסטודנטים להגיע אל היעד/התעודה שהוגדרו. עבור כל סטודנט/ית שיסיימו אחד מאפיקי הלימודים תתבצע בדיקת עמידה בתנאי הסיום של התכנית. ",
